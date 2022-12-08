1fce8bed5edd56bff8cc90572c5b2a47ff97552c
Nadim Chowdhury <40499378+Nadim-Chowdhury@users.noreply.github.com>
2022-12-08 19:30:20 +0600

    Add files via upload

---
+++ 0. Random Projects/Animated Temp/script.js
@@ -0,0 +1,26 @@
+const tempLoad = () => {
+    let temp = document.getElementById('temp');
+    temp.innerHTML = "&#xf2cb";
+    temp.style.color = "#f8bc27";
+
+    setTimeout(() => {
+        temp.innerHTML = "&#xf2ca";
+    }, 1000);
+
+    setTimeout(() => {
+        temp.innerHTML = "&#xf2c9";
+    }, 2000);
+
+    setTimeout(() => {
+        temp.innerHTML = "&#xf2c8";
+    }, 3000);
+
+    setTimeout(() => {
+        temp.innerHTML = "&#xf2c7";
+        temp.style.color = "#d63031";
+    }, 4000);
+}
+
+tempLoad();
+
+setInterval(tempLoad, 5000);
---
+++ 0. Random Projects/Cool Shutter Navigation/script.js
@@ -0,0 +1,4 @@
+$('.icon').click(function () {
+    $('.left').toggleClass('left-active');
+    $('.right').toggleClass('right-active');
+})
